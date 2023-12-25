ff7ddce92835072c6cd64fd8129a6d7a0a24cb8c
MultiLayer <153393440+Minamotion@users.noreply.github.com>
2023-12-25 17:45:38 -0600

    Fixed banners

--- resources/scripts/banners.js
+++ resources/scripts/banners.js
@@ -1,16 +1,21 @@
-let i = 0;
-let json_parsed = []
-fetch('/Minamotion/resources/data/banners.json')
-    .then(response => response.json())
-    .then(data => json_parsed = data)
-let curContent = null;
+async function loadBanners() {
+    let i = 0;
+    let json_parsed = []
+    let curContent = null;
+
+    await fetch('/Minamotion/resources/data/banners.json')
+        .then(response => response.json())
+        .then(data => json_parsed = data)
   
-while (i > Object.keys(json_parsed).length) {
-    curContent = document.getElementById('container').innerHTML
-    document.getElementById('container').innerHTML = curContent+'<iframe src="'+json_parsed[i].url+'" width="450" height="250" id="banner_'+json_parsed[i].name+'" class="banner"></iframe>'
-    i++
+    while (i > Object.keys(json_parsed).length) {
+        curContent = document.getElementById('container').innerHTML
+        document.getElementById('container').innerHTML = curContent+'<iframe src="'+json_parsed[i].url+'" width="450" height="250" id="banner_'+json_parsed[i].name+'" class="banner"></iframe>'
+        i++
+    }
+
+    delete json_parsed
+    delete curContent
+    delete i
 }
 
-delete json_parsed
-delete curContent
-delete i
+loadBanners()

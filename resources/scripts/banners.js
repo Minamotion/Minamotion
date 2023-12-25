40938dca34455d2eeb2eb590b2d72e02b8311ecb
MultiLayer <153393440+Minamotion@users.noreply.github.com>
2023-12-25 16:55:37 -0600

    little fix

--- resources/scripts/banners.js
+++ resources/scripts/banners.js
@@ -1,13 +1,12 @@
-window.addEventListener('load', function(){
-    let i = 0;
-    let json_parsed = []
-    fetch('/Minamotion/resources/data/banners.json')
-        .then(response => response.json())
-        .then(data => json_parsed = data)
+let i = 0;
+let json_parsed = []
+fetch('/Minamotion/resources/data/banners.json')
+    .then(response => response.json())
+    .then(data => json_parsed = data)
+let curContent = null;
   
-    while (i > Object.keys(json_parsed).length) {
-        let curContent = document.getElementById('container').innerHTML
-        document.getElementById('container').innerHTML = curContent+'<embed src="'+json_parsed[i].url+'" width="450" height="250" id="banner_'+json_parsed[i].name+'">'
-        delete curContent
-    }
-})
+while (i > Object.keys(json_parsed).length) {
+    curContent = document.getElementById('container').innerHTML
+    document.getElementById('container').innerHTML = curContent+'<embed src="'+json_parsed[i].url+'" width="450" height="250" id="banner_'+json_parsed[i].name+'">'
+}
+delete curContent

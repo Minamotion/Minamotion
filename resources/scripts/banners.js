8c0e8512f60843d6aacff38b613d0504ebbd8dd0
MultiLayer <153393440+Minamotion@users.noreply.github.com>
2023-12-25 18:12:14 -0600

    pls work

--- resources/scripts/banners.js
+++ resources/scripts/banners.js
@@ -1,19 +1,16 @@
 async function loadBanners() {
     let i = 0;
-    let json_parsed = []
-    let curContent = null;
-
-    await fetch('/Minamotion/resources/data/banners.json')
-        .then(response => response.json())
-        .then(data => json_parsed = data)
+    let json = []
+    let curContent = "";
+    await fetch('/Minamotion/resources/data/banners.json').then(response => response.json()).then(data => json = data).catch(error => console.error('Error at "banners.json": Catched javascript error "'+error+'"'))
   
-    while (i > Object.keys(json_parsed).length) {
+    while (i > Object.keys(json).length) {
         curContent = document.getElementById('container').innerHTML
-        document.getElementById('container').innerHTML = curContent+'<iframe src="'+json_parsed[i].url+'" width="450" height="250" id="banner_'+json_parsed[i].name+'" class="banner"></iframe>'
+        document.getElementById('container').innerHTML = curContent+'<iframe src="'+json[i]+'" width="450" height="250" class="banner"></iframe>'
         i++
     }
 
-    delete json_parsed
+    delete json
     delete curContent
     delete i
 }

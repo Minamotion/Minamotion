8bfca25e0bcea0f6c1f8f3853b0bb786d0c43dc8
MultiLayer <multilayernowofficial@gmail.com>
2024-01-01 23:43:44 -0600

    fix in lag

--- assets/scripts/mistaken.js
+++ assets/scripts/mistaken.js
@@ -1,8 +1,12 @@
-let repoArray = ["congratscard"]
+let repoArray = ["congratscard", "Minamotion", "thegameplace"]
 let idx = 0
 
 while (idx < Object.keys(repoArray).length) {
     if("/"+repoArray[idx] === this.location.pathname){
         this.location.assign("minamotion.github.io/"+repoArray[idx])
+        break;
     }
+    idx++
 }
+
+console.warn('If you\'re searching for a repository use "minamotion.github.io" instead of "minamotion.org"')

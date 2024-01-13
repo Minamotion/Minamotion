c92125edb7f99575d5d9e2cd083d1bba9da4b3e1
MultiLayer <multilayernowofficial@gmail.com>
2024-01-13 14:24:20 -0600

    Create safety.js
    I made this script which redirects you to the https protocol

---
+++ assets/scripts/safety.js
@@ -0,0 +1,2 @@
+// I want users to have a secure connection so I made this script
+if (window.location.protocol !== 'https:') {localStorage.clear();sessionStorage.clear();setTimeout(() => {window.location.assign('https://'+window.location.host+window.location.pathname+window.location.search)},10);}

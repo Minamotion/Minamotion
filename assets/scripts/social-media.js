ec6ecd88592a076fe94a9a59a3c769c796736c34
MultiLayer <multilayernowofficial@gmail.com>
2024-12-16 16:57:28 -0600

    unfulllinkification entry

--- assets/scripts/social-media.js
+++ assets/scripts/social-media.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",async ()=>{
-    await fetch("https://minamotion.github.io/Minamotion/assets/data/social-media-links.json").then(response=>response.json()).then((array)=>{
+    await fetch("/assets/data/social-media-links.json").then(response=>response.json()).then((array)=>{
         array.forEach((data)=>{
-            const element = `<a href="${data.link}" title="${data.title}"><img src="https://minamotion.github.io/Minamotion/assets/images/link-icons/${data.icon}.png" width=100></img></a>`
+            const element = `<a href="${data.link}" title="${data.title}"><img src="/assets/images/link-icons/${data.icon}.png" width=100></img></a>`
             document.getElementById("socialmedia").innerHTML += element
         })
     })
